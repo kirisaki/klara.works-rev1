03fd71e5c24c0f05b39fcac752988db406aabcc6
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-04-18 14:41:40 +0900

    feat: style navigation

--- client/src/components/About.tsx
+++ client/src/components/About.tsx
@@ -5,10 +5,11 @@ export const About: React.FC = () => {
   return (
     <article>
       <h1>
-        Information
+        About
       </h1>
       <section>
-        nyaan
+        このサイトは同人サークル「Klara Works（クラーラ・ワークス）」および霧咲空人（きりさきあきひと）の個人ポートフォリオサイトです。
+        過去作品の掲載や情報発信行っていく予定です。また、Web技術試行の場ともなっております。
       </section>
     </article>
   )
---
+++ client/src/components/NavLink.tsx
@@ -0,0 +1,24 @@
+import React, {} from 'react'
+import { Link, useRouteMatch } from 'react-router-dom'
+
+import style from './NavLink.css'
+
+type Props = {
+  to: string
+  children: string,
+  exact?: boolean,
+}
+
+const NavLink: React.FC<Props> = ({to, children, exact}) => {
+  const match = useRouteMatch({path: to, exact})
+  return (
+    <Link
+      to={to}
+      className={match ? [style.link, style.active].join(' ') : style.link}>
+      <div>{children}</div>
+    </Link>
+  )
+}
+
+export default NavLink
+
