c39d7f49266be42fd86cd95c2b169c7aad2e4a61
Akihito KIRISAKI <kirisaki@klaraworks.net>
2020-04-19 21:51:38 +0900

    feat: fetch API

--- client/src/components/Works.tsx
+++ client/src/components/Works.tsx
@@ -1,14 +1,23 @@
-import React, {} from 'react'
+import React, { useEffect, useState } from 'react'
 
 import style from './Works.css'
 import { usePage } from '../hooks/page'
+import { req } from '../lib/req'
+import { Work } from '../types'
 
 export const Works: React.FC = () => {
   usePage('Works')
+  const [works, setWorks] = useState<Work[]>([])
+  useEffect(() => {
+    const f = async () => {
+      const result = await req<Work[]>('/api/v0/works/jpn')
+      setWorks(result)
+    }
+    f()
+  }, [])
   return (
     <article className={style.about}>
-      <section>
-      </section>
+      <div>{works.map(work => work.id)}</div>
     </article>
   )
 }
